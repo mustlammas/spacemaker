7e7c8f1c36cfb9fdee0f5d091f3a4d5cf5077841
Marko Kurm <marko.kurm@gmail.com>
2018-05-29 21:55:29 +0300

    Get features from server.

--- app/src/app.js
+++ app/src/app.js
@@ -1,6 +1,5 @@
 var connect = require('connect');
 var serveStatic = require('serve-static');
-var __dirname = 'public';
-connect().use(serveStatic(__dirname)).listen(3000, function(){
+connect().use(serveStatic('public')).listen(3000, function(){
     console.log('Server running on 3000...');
 });
