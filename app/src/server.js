b80e17de8f8ff6f5414e3e12b4bb20f6a44437ba
Marko Kurm <marko.kurm@gmail.com>
2018-05-30 18:23:40 +0300

    Serve static files from the public directory. Remove unused files.

--- app/src/server.js
+++ app/src/server.js
@@ -1,12 +1,10 @@
+var express = require('express');
 var connect = require('connect');
 var serveStatic = require('serve-static');
 var app = require('./app');
 
-/*connect().use(serveStatic('./src/public')).listen(3000, function(){
-    console.log('Server running on 3000...');
-});*/
-
 var port = 3000;
+app.use(express.static('./src/public'));
 var server = app.listen(port, function() {
   console.log('Express server listening on port ' + port);
 });
